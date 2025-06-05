eda23fe9d10beebe5dc5bedb3065e0a1c079c270
siddhesh <siddheshbadnapurkar@gmail.com>
2025-06-05 14:51:19 +0530

    created enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug)]
+enum Expr {
+    Number(f64), // 2.0 or 3.4
+    Variable(String), // x or y
+    Add(Box<Expr>, Box<Expr>) // x + 2.3 or 2.4 + 2.1
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    // Create an expression like x + 2.5
+    let expr: Expr = Expr::Add(
+        Box::new(Expr::Variable("x".to_string())),
+        Box::new(Expr::Number(2.5))
+    );
+
+    println!("{:?}", expr); // Outputs Add(Variable("x"), Number(2.5))
 }
