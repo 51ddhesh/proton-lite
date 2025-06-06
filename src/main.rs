b1af6d6ee8095f3741a65594968857147943e839
Siddhesh Badnapurkar <siddheshbadnapurkar@gmail.com>
2025-06-06 12:16:23 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     // Contents have been moved to examples/example.rs
-    // This file is kept in case of using this engine as an API service
+    // This file is kept in case of using this engine as a CLI or an API service
 }
