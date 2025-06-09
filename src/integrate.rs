8f34d0347c90275ae7443336813390852b1e81dc
siddhesh <siddheshbadnapurkar@gmail.com>
2025-06-10 03:04:59 +0530

    added basic integrations

--- src/integrate.rs
+++ src/integrate.rs
@@ -1,5 +1,36 @@
 use crate::expr::Expr;
 
-pub fn integrate(_expr: &Expr, _var: &str) -> Expr {
-    todo!{"Implement symbolic integration"};
+pub fn integrate(expr: &Expr, var: &str) -> Expr {
+    match expr {
+        // ∫c dx = cx + k
+        Expr::Number(c) => {
+            Expr::Mul(
+                Box::new(Expr::Number(*c)),
+                Box::new(Expr::Variable(var.to_string()))
+            )
+        },
+
+        Expr::Variable(x) if x == var => {
+            // ∫x dx = x^2 / 2
+            Expr::Div(
+                Box::new(
+                    Expr::Pow(Box::new(Expr::Variable(var.to_string())), Box::new(Expr::Number(2.0)))
+                ),
+                Box::new(Expr::Number(2.0)),
+            ) 
+        },
+
+        // ∫(f + g) = ∫f + ∫g
+        Expr::Add(left, right) => {
+            Expr::Add(
+                Box::new(integrate(&left, var)),
+                Box::new(integrate(&right, var))
+            )
+        },
+
+        
+
+        _ => todo!()
+
+    }
 }
